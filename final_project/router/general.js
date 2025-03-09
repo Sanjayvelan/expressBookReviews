d16a1aa29b32a6a7edb36728cbd3dd9cf3641a1e
Sanjayvelan <sanjayraj1018@gmail.com>
2025-03-09 12:30:57 +0530

    Implemented book search by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,10 +50,26 @@ public_users.get('/author/:author', function (req, res) {
 
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get all books based on title
+public_users.get('/title/:title', function (req, res) {
+  let title = req.params.title; // Extract title from request params
+  let booksByTitle = [];
+
+  // Iterate through books object
+  for (let key in books) {
+      if (books[key].title.toLowerCase() === title.toLowerCase()) {
+          booksByTitle.push({ isbn: key, author: books[key].author });
+      }
+  }
+
+  // Check if books were found
+  if (booksByTitle.length > 0) {
+      return res.status(200).json({ books: booksByTitle });
+  } else {
+      return res.status(404).json({ message: "No books found with this title" });
+  }
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
